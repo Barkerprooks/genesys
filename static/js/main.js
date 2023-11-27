f9f7eff427801fc449e0af7b94dc2e102374d9e4
Parker Brooks <jonparkerbrooks@gmail.com>
2023-11-27 16:40:57 -0600

    actually now its done

--- static/js/main.js
+++ static/js/main.js
@@ -12,16 +12,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const { width, height } = canvas;
 
     button.onclick = () => {
+        context.fillStyle = 'white';
+        context.fillText('Loading...', (width / 2) - 20, height / 2);
         fetch(`/galaxy/create?n=${nInput.value}&d=${dInput.value}&phi=${phiInput.value}`, { credentials: "include" })
             .then(response => response.json())
             .then(systems => {
                 context.clearRect(0, 0, width, height);
                 systems.forEach(system => {
                     let [ x, y ] = system.coordinates;
-                    x += width / 2;
-                    y += height / 2;
                     context.fillStyle = randomChoice(['red', 'green', 'blue']);
-                    context.fillRect(x, y, 1, 1);
+                    context.fillRect(x + (width / 2), y + (height / 2), 1, 1);
                 });
             })
             .catch(error => alert(error));
